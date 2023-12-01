dc55d704b127f092b9acaf3af054ad0b380190aa
Mike Shi <dongming.shi@uqconnect.edu.au>
2023-12-01 13:24:00 +1000

    added test script for coral ai edgetpu

---
+++ coral/README.md
@@ -0,0 +1,86 @@
+# PyCoral API examples
+
+This directory contains several examples that show how to use the
+[PyCoral API](https://coral.ai/docs/edgetpu/api-intro/) to perform
+inference or on-device transfer learning.
+
+## Run the example
+
+1.  [Set up your Coral device](https://coral.ai/docs/setup/) (includes steps
+to install this PyCoral library).
+
+2.  Clone this repo onto the host device (onto your Coral board, or if using
+a Coral accelerator, then onto the host system):
+
+    ```
+    git clone https://github.com/NeuroRehack/PhysiCam.git
+    ```
+
+3.  Download the required model and other files required to run each sample,
+using the `install_requirements.sh` script. Pass this script the filename you
+want to run and it will download just the files for that example. Or exclude the
+filename to download the files for all examples:
+
+    ```
+    bash install_requirements.sh
+    ```
+
+4.  Connect USB Accelerator to the host device.
+
+5.  Then run the example command shown at the top of the `.py` file to run
+the code (using the files you just downloaded). 
+
+<!--
+Some examples also require
+additional downloads, which are specified in the code comments at the top of the
+file.
+-->
+
+## Movenet Pose Estimation
+
+### Movenet Landmarks
+```
+0: 'nose'
+1: 'left_eye'
+2: 'right_eye'
+3: 'left_ear'
+4: 'right_ear'
+5: 'left_shoulder'
+6: 'right_shoulder'
+7: 'left_elbow'
+8: 'right_elbow'
+9: 'left_wrist'
+10: 'right_wrist'
+11: 'left_hip'
+12: 'right_hip'
+13: 'left_knee'
+14: 'right_knee'
+15: 'left_ankle'
+16: 'right_ankle'
+```
+
+### Example Output
+```
+[0.47367048 0.6101518  0.5017696 ]
+[0.42148647 0.6221943  0.3733166 ]
+[0.42951477 0.59409523 0.5017696 ]
+[0.46162802 0.6302226  0.18063706]
+[0.4375431  0.51381207 0.26493436]
+[0.71853405 0.59409523 0.7345907 ]
+[0.66635    0.38134488 0.5017696 ]
+[0.9593835  0.61818016 0.5017696 ]
+[0.96741176 0.30909008 0.3733166 ]
+[0.32916087 0.6061377  0.18063706]
+[0.32916087 0.5258545  0.18063706]
+[1.0115675  0.6462792  0.11641055]
+[1.0035392  0.45359972 0.11641055]
+[0.7305765  0.59810936 0.04816988]
+[0.75867563 0.5419112  0.01204247]
+[0.36930242 0.5860669  0.18063706]
+[0.28500512 0.5459253  0.18063706]
+```
+
+For more pre-compiled models, see [coral.ai/models](https://coral.ai/models/).
+
+For more information about building models and running inference on the Edge
+TPU, see the [Coral documentation](https://coral.ai/docs/).
---
+++ coral/pose_estimation_video.py
@@ -0,0 +1,155 @@
+# Lint as: python3
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Example using PyCoral to estimate a single human pose with Edge TPU MoveNet.
+
+To run this code, you must attach an Edge TPU to the host and
+install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`. For
+device setup instructions, see coral.ai/docs/setup.
+
+For more details about MoveNet and its best practices, please see
+https://www.tensorflow.org/hub/tutorials/movenet
+
+Example usage: py [-3.7 | -3.8 | -3.9] pose_estimation_video.py
+
+Modified by: Mike Smith
+"""
+
+"""
+movenet landmarks
+0: 'nose', 1: 'left_eye', 2: 'right_eye', 3: 'left_ear', 4: 'right_ear',
+5: 'left_shoulder', 6: 'right_shoulder', 7: 'left_elbow', 8: 'right_elbow',
+9: 'left_wrist', 10 : 'right_wrist', 11: 'left_hip', 12: 'right_hip', 
+13: 'left_knee', 14: 'right_knee', 15: 'left_ankle', 16: 'right_ankle'
+"""
+
+import sys, time, os
+
+from pycoral.adapters import common
+from pycoral.utils.edgetpu import make_interpreter
+
+import cv2 as cv
+from statistics import mean
+
+NUM_KEYPOINTS = 17
+
+VIS_THRESH = 0.5
+
+KEYPOINT_CONNECTIONS = [
+    (0, 1), (0, 2), (1, 3), (2, 4), (5, 7), (7, 9), (6, 8), (8, 10), (5, 6),
+    (5, 11), (6, 12), (11, 12), (11, 13), (13, 15), (12, 14), (14, 16),
+]
+
+def display_frame_rate(img, frame_times):
+    frame_times["curr time"] = time.time()
+    frame_rate = 1 / (frame_times["curr time"] - frame_times["prev time"])
+    frame_times["prev time"] = frame_times["curr time"]
+
+    img = cv.flip(img, 1)
+    cv.putText(img, str(int(frame_rate)), (10, 30), 0, 1, (255, 255, 0))
+    img = cv.flip(img, 1)
+    return img
+
+
+def main():
+    model_path = "models/movenet/movenet_single_pose_thunder_ptq_edgetpu.tflite"
+    #model_path = "models/posenet/mobilenet/posenet_mobilenet_v1_075_481_641_16_quant_decoder_edgetpu.tflite"
+    #model_path = "models/posenet/resnet/posenet_resnet_50_416_288_16_quant_edgetpu_decoder.tflite"
+
+    interpreter = make_interpreter(model_path)
+    interpreter.allocate_tensors()
+
+    """ open video or webcam """
+    #cap = cv.VideoCapture("path_to_video.mp4")
+    cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
+
+    """ frame rate (for debugging) """
+    frame_times = {"curr time": 0, "prev time": 0}
+
+    lpf_buf_x = {key: list() for key in range(17)}
+    lpf_buf_y = {key: list() for key in range(17)}
+    lpf_buf_vis = {key: list() for key in range(17)}
+    lpf_buf_len = 5
+    x_mean, y_mean, vis_mean = 0, 0, 0
+
+    while cap.isOpened():
+        ret, img = cap.read()
+        if ret == False:
+            print("can't connect to camera :(")
+            break
+
+        """ display frame rate """
+        img = display_frame_rate(img, frame_times)
+
+        interpreter_size = common.input_size(interpreter)
+        resized_img = cv.resize(img, (interpreter_size[0], interpreter_size[1]), interpolation=cv.INTER_AREA)
+        common.set_input(interpreter, resized_img)
+        interpreter.invoke()
+
+        pose = common.output_tensor(interpreter, 0).copy()
+        pose = pose.reshape(17, 3)
+        lpf_pose = list()
+        
+        h, w, _ = img.shape
+        for i, lm in enumerate(pose):
+            y, x, vis = lm
+
+            if vis > 0.5:
+                lpf_buf_x[i].append(x)
+                lpf_buf_y[i].append(y)
+                lpf_buf_vis[i].append(vis)
+
+            if len(lpf_buf_x[i]) >= lpf_buf_len:
+                lpf_buf_x[i] = lpf_buf_x[i][-lpf_buf_len:]
+                x_mean = mean(lpf_buf_x[i])
+
+            if len(lpf_buf_y[i]) >= lpf_buf_len:
+                lpf_buf_y[i] = lpf_buf_y[i][-lpf_buf_len:]
+                y_mean = mean(lpf_buf_y[i])
+
+            if len(lpf_buf_vis[i]) >= lpf_buf_len:
+                lpf_buf_vis[i] = lpf_buf_vis[i][-lpf_buf_len:]
+                vis_mean = mean(lpf_buf_vis[i])
+                
+            cv.circle(img, (int(x_mean*w), int(y_mean*h)), 8, (0, 0, 255), cv.FILLED)
+            lpf_pose.append([int(x_mean*w), int(y_mean*h)])
+
+
+        for start, end in KEYPOINT_CONNECTIONS:
+            start_x, start_y = int(lpf_pose[start][0]), int(lpf_pose[start][1])
+            end_x, end_y = int(lpf_pose[end][0]), int(lpf_pose[end][1])
+            cv.line(img, (start_x, start_y), (end_x, end_y), (255, 255, 255), 2)
+        
+
+        """ flip frame horizontally """
+        img = cv.flip(img, 1)
+
+        """ show image """
+        cv.imshow("Image", img)
+
+        """ press "q" to exit program """
+        key = cv.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    cv.destroyAllWindows()
+    cap.release()
+
+
+if __name__ == '__main__':
+  main()
